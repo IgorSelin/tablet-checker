f2295d2141149af75f746d141f19e954127162d3
IgorSelin <67583045+IgorSelin@users.noreply.github.com>
2023-01-26 13:01:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const userAgent = navigator.userAgent.toLowerCase();
+const isTablet =
+  /(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(
+    userAgent
+  );
+alert(`Is tablet: ${isTablet}`);
